1b1037b9ac150930a5855536e19b8cbebb5a217e
wuxiang <voff12@gmail.com>
2020-11-06 08:49:58 +0800

    fixed some NPE

---
+++ src/test/java/com/allibaba/compileflow/test/mock/TestRunTime.java
@@ -0,0 +1,29 @@
+package com.allibaba.compileflow.test.mock;
+
+import com.alibaba.compileflow.engine.definition.tbbpm.TbbpmModel;
+import com.alibaba.compileflow.engine.runtime.impl.TbbpmStatelessProcessRuntime;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.when;
+
+public class TestRunTime {
+
+
+    /**
+     * 测试代码中的异常
+     *
+     */
+    @Test(expected=IllegalArgumentException.class)
+    public void test_process_runtime() {
+
+        TbbpmModel model = Mockito.mock(TbbpmModel.class);
+        TbbpmStatelessProcessRuntime runtime = TbbpmStatelessProcessRuntime.of(model);
+        when(runtime.generateTestCode()).thenReturn("hello world");
+        System.out.println(runtime.generateTestCode());
+        runtime.registerNodeGenerator(null);
+
+
+
+    }
+}
