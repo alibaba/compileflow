c65bb2ea7c2e2a7015b614ec221b44f813d347b8
Jerry Lee <oldratlee@gmail.com>
2021-07-14 16:32:42 +0800

    ! move newVersion.sh -> scripts/bump-version.sh

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
+set -eEuo pipefail
+
+cd "$(dirname "$(readlink -f "$0")")"/.. || {
+    echo "Fail to change work dir!" 1>&2
+    exit 1
+}
+
 if [ $# != 1 ]; then
     {
-        echo "Only 1 argument for verson!"
+        echo "Only 1 argument for version!"
         echo
         echo "usage: $0 <new version>"
     } 1>&2
 
-	exit 1
+    exit 1
 fi
 
 mvn versions:set -DgenerateBackupPoms=false -DnewVersion="$1"
